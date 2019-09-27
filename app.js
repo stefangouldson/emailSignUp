6ffd70c959d7cb8b5e8da15e0d897f29b0c0c8a0
CN0103 <cn0103@CN103.local>
2019-09-27 15:53:10 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const mysql = require ('mysql');
+const {promisify}=require ('util');
+// const faker=require ('faker');
+
+const connection = mysql.createConnection ({
+    host:"localhost",
+    user:"root",
+    password:"StEfAn20192792",
+    database:"join_us"
+});
+
+const promisifiedQuery=promisify(connection.query).bind(connection);
+
+
+const runQuery = async () =>{
+    try{
+        let data = await promisifiedQuery('SELECT * FROM users');
+        return data;
+    } catch (error) {
+        console.log(error.sqlMessage);
+    }
+    connection.end()
+}
+
+
+const addEmail = async (email) => {
+    try{
+        const queryStringAdd = `INSERT INTO users(email) VALUES ('${email}')`;
+        let data = await promisifiedQuery(queryStringAdd);
+        return (data);
+    } catch (error) {
+        console.log(error.sqlMessage);
+    }
+}
+
+// const person ={
+//     email: faker.internet.email()
+// };
+
+// const bulkAdd =() =>{
+//     let people =[];
+//     for (i=0;i<500;i++){
+//         people.push([faker.internet.email(), faker.date.past()])
+//     }
+//     return people;
+// }
+
+
+addEmail();
+
+module.exports={
+    runQuery,
+    addEmail
+};
